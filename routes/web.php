e88fc0c0fb4feda27787ee021e3a212a9cc11762
Robert Ebafua <robertebafua@gmail.com>
2024-08-04 22:31:12 +0100

    create admin account url

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,33 @@
 <?php
 
+use App\Http\Controllers\Auth\ConfirmPasswordController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ReportController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'layouts.index')->name('index');
 Route::post('/report', [ReportController::class, 'store'])->name('report');
 
 Route::group(['prefix' => 'admin/auth'], function () {
-    Auth::routes();
+    Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('login', [LoginController::class, 'login']);
+    Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
+// Registration Routes...
+    Route::get('snap/create-admin', [RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('snap/create-admin', [RegisterController::class, 'register']);
+
+//Password Confirm
+    Route::get('password/confirm', [ConfirmPasswordController::class, 'showConfirmForm'])->name('password.confirm');
+    Route::post('password/confirm', [ConfirmPasswordController::class, 'confirm']);
+
+// Password Reset Routes...
+    Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+    Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+    Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+    Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
